6839483843bd7e425432ad83bf24b295fb30dd91
lil <li@last.nl>
2017-12-07 13:18:44 +0100

    Tests 100% of cryptor in nyc

---
+++ test/cryptor.spec.js
@@ -0,0 +1,37 @@
+import test from 'ava'
+import SimpleCryptoJS from 'simple-crypto-js'
+import cryptor, { isIgnore } from '../src/cryptor'
+
+const ignore = ['_id', '_rev', '_deleted']
+
+test('cryptor', t => {
+  const password = 'password'
+  const inCrypto = new SimpleCryptoJS(password)
+  const outCrypto = new SimpleCryptoJS(password)
+
+  let doc = {
+    _id: 'thisid',
+    one: [
+      {text: 'hello'},
+      {text: 'goodbye'},
+    ],
+    two: 2,
+    three: 'hi',
+    four: 3e4,
+    five: [['one'], ['two'], ['three', 'four']],
+  }
+
+  const encrypted = cryptor(inCrypto, doc, ignore, true)
+  t.log(JSON.stringify(encrypted))
+  const decrypted = cryptor(outCrypto, encrypted, ignore, false)
+
+  t.deepEqual(doc, decrypted, 'if cryptor works for Array<object>')
+})
+
+test('isIgnore', t => {
+  t.true(ignore[0] === '_id')
+  t.true(
+    isIgnore(ignore, '_id'),
+    '"_id" true'
+  )
+})
