8dc439003d98e17273188c148f1c8366ec45c820
lil <li@last.nl>
2017-12-09 17:10:49 +0100

    Test change password %100

--- test/index.spec.js
+++ test/index.spec.js
@@ -20,7 +20,16 @@ function checkResponse (t, id, response) {
   t.is(response.id, id)
 }
 
+function removeFile (filepath) {
+  try {
+    fs.unlinkSync(filepath)
+  } catch (err) {
+    if (err.code !== 'ENOENT') throw err
+  }
+}
+
 test('plugin install', t => {
+  removeFile(`${dbPath}/install.db`)
   const db = pouchStart(t, 'install')
   db.simplecryptor('password')
 
@@ -33,7 +42,7 @@ test('plugin install', t => {
 })
 
 test('empty db put, get', t => {
-  fs.unlinkSync(`${dbPath}/empty.db`)
+  removeFile(`${dbPath}/empty.db`)
   const db = pouchStart(t, 'empty')
   db.simplecryptor('password')
 
@@ -56,4 +65,65 @@ test('empty db put, get', t => {
         })
         .catch(err => t.fail(err))
     })
+})
+
+test('change password', t => {
+  // remove from last test to have clean tests
+  removeFile(`${dbPath}/pass-original.db`)
+  removeFile(`${dbPath}/pass-changed.db`)
+
+  // create two databases
+  const originalDB = pouchStart(t, 'pass-original')
+  const changedDB = pouchStart(t, 'pass-changed')
+
+  // add simplecryptor
+  originalDB.simplecryptor('old')
+  changedDB.simplecryptor('n3wbe//erpassword')
+
+  const exampleDoc = {
+    _id: 'firstput',
+    name: 'fee',
+  }
+
+  // put exampleDoc in db original
+  return originalDB.put(exampleDoc)
+    .then(response => {
+      checkResponse(t, exampleDoc._id, response)
+
+      // get exampleDoc from db original
+      return originalDB.get(exampleDoc._id)
+        .then(doc => {
+          t.deepEqual({
+            _id: doc._id,
+            name: doc.name,
+          }, exampleDoc)
+
+          // copy original to changed
+          return new Promise((resolve, reject) => {
+            PouchDB
+              .replicate(originalDB, changedDB,
+                {live: false, retry: false})
+              .on('complete', info => resolve({output: info, message: 'complete'}))
+              .on('error', err => reject(Error({output: err, message: 'error'})))
+              .on('denied', err => reject(Error({output: err, message: 'denied'})))
+          })
+            .then(response => {
+              t.log(JSON.stringify(response.output))
+              t.is(response.message, 'complete')
+              t.true(response.output.ok)
+
+              // get exampleDoc from changed
+              return changedDB.get(exampleDoc._id)
+                .then(doc => {
+                  t.deepEqual({
+                    _id: doc._id,
+                    name: doc.name,
+                  }, exampleDoc)
+                })
+                .catch(err => t.fail(err))
+            })
+            .catch(err => t.fail(err))
+        })
+    })
+    .catch(err => t.fail(err))
 })
